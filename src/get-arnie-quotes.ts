81711508e0baad5f37697739d7917e103da6db17
Ruth Wang <ruth.wang@fluxfederation.com>
2024-05-05 17:19:59 +1000

    Implement getArnieQuotes to pass unit tests, add types

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,23 @@
 import { httpGet } from './mock-http-interface';
 
 // TODO define this type properly
-type TResult = any;
+type TResult = {
+  'Arnie Quote'?: string;
+  'FAILURE'?: string;
+}
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
   // TODO: Implement this function.
   
-  return [];
+  const results = urls.map(async (url): Promise<TResult> => {
+    try {
+      const { status, body } = await httpGet(url);
+      const message =  JSON.parse(body).message;
+      return (status === 200) ? { 'Arnie Quote': message } : { 'FAILURE': message };
+    } catch (error: any) {
+      return { 'FAILURE': error.message || 'Request failed' };
+    }
+  });
+
+  return Promise.all(results);
 };
